eaf4c42003eef7fa6d71acf96763e5d29f1779a9
jlbevans <jacob-bevans@lambdastudents.com>
2021-06-27 14:30:56 -0600

    i have completed the framework for this website

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,77 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components'
+
+const Character = ({ name, gender, birth_year, hair, eyes, skin, weight, height }) => {
+
+
+    return(
+        <Container>
+            <Head>{name}</Head>
+            <SubHead>{`Gender: ${gender}`}</SubHead>
+            <SubHead>{`Born: ${birth_year}`}</SubHead>
+            <DetailsDiv>
+            <TopList>
+            <ListItem>{`Weight: ${weight}kg`}</ListItem>
+            <ListItem>{`Height: ${height}`}</ListItem>
+            </TopList>
+            <BottomList>
+            <ListItem>{`Hair Color: ${hair}`}</ListItem>
+            <ListItem>{`Eye Color: ${eyes}`}</ListItem>
+            <ListItem>{`Skin Color: ${skin}`}</ListItem>
+            </BottomList>
+            </DetailsDiv>
+        </Container>
+    )
+
+}
+
+const Container = styled.div`
+    background-color: #e6cc87;
+    display: inline-block;
+    padding: 1.25%;
+    margin: 1%;
+    justify-content: center;
+    width: 80%;
+    border: .125rem;
+    border-radius: 12px;
+`
+
+const Head = styled.h1`
+    font-size: 3rem;
+    text-align: left;
+    padding: 1.5%;
+    margin: 1%;
+    color: white;
+    border-radius: 8px;
+    background-color: black
+`
+
+const SubHead = styled.h3`
+    font-size: 1.25rem;
+    text-align: left;
+    margin: 1%
+`
+
+const DetailsDiv = styled.div`
+    font-size: 0.75rem;
+    text-align: center;
+    margin: 1%
+`
+
+const ListItem = styled.li`
+    text-decoration: none
+`
+
+const TopList = styled.ul`
+    list-style: none;
+    font-size: 0.95rem;
+`
+
+const BottomList = styled.ul`
+    list-style: none;
+    font-size: 0.75rem;
+`
+
+export default Character
+
